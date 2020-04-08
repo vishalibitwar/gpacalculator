ee71fc71dc10b83061ee201542ae87d7ffa6227c
vishalibitwar <vish31ibitwar@gmail.com>
2020-04-08 17:32:26 +0530

    Updates

---
+++ precache-manifest.a0b064edda24b2a645988f08815e2458.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b00a86005b34776040e7f0c2c23cd37",
+    "url": "/gpacalculator/index.html"
+  },
+  {
+    "revision": "76c6cd297213329177b1",
+    "url": "/gpacalculator/static/css/main.d2e211a3.chunk.css"
+  },
+  {
+    "revision": "84d44b5abebe84c8b160",
+    "url": "/gpacalculator/static/js/2.73e87614.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gpacalculator/static/js/2.73e87614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c6cd297213329177b1",
+    "url": "/gpacalculator/static/js/main.f68d1a17.chunk.js"
+  },
+  {
+    "revision": "58dd11c47182451d9e7e",
+    "url": "/gpacalculator/static/js/runtime-main.30a756cb.js"
+  }
+]);
